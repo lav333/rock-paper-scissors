fa6a3acb54c4c24c2443071d4289bea26bb43166
lav333 <val.dragasevic@gmail.com>
2024-08-30 20:52:11 +0200

    Finish game in console

--- script.js
+++ script.js
@@ -27,5 +27,62 @@ function getHumanChoice() {
     }
 }
 
-console.log("Computer choice: " + getComputerChoice());
-console.log("Human choice: " + getHumanChoice())
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice == computerChoice) {
+        return "Tie!\nhuman: " + humanScore + " computer: " + computerScore
+    }
+    else if (winner(humanChoice, computerChoice) == "a") {
+        humanScore += 1;
+        return "Human wins!\nhuman: " + humanScore + " computer: " + computerScore
+    }
+    else if (winner(humanChoice, computerChoice) == "b") {
+        computerScore += 1
+        return "Computer wins!\nhuman: " + humanScore + " computer: " + computerScore
+    }
+}
+
+function winner(a, b) {
+    if (a != b) {
+        if ((a == "rock" && b == "scissors") || (a == "paper" && b == "rock") || (a == "scissors" && b == "paper")) {
+            return "a";
+        }
+        else {
+            return "b";
+        }
+    }
+}
+
+function playGame() {
+    humanScore = 0;
+    computerScore = 0;
+    for(let i = 0; i<5; i++) {
+        const human = getHumanChoice();
+        const computer = getComputerChoice();
+        console.log(playRound(human, computer));
+        console.log(human);
+        console.log(computer);
+    }
+
+    if (humanScore == computerScore) {
+        return "Game over! It's a tie!";
+    }
+    else if (humanScore > computerScore) {
+        return "Game over! Human wins!";
+    }
+    else {
+        return "Game over! Computer wins!";
+    }
+}
+
+console.log(playGame());
+
+let count = 1;
+while (count == 1) {
+    let newGame = prompt("Another game? Yes/No");
+    if (newGame == "Yes") {
+        console.log(playGame());
+    }
+    else {
+        count = 0
+    }
+}
